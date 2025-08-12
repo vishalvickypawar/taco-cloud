3990c3103a7e9eee0c2b72e933f7dff0bdafaca9
Vishal Pawar <168279073+vishalvickypawar@users.noreply.github.com>
2025-08-12 22:14:02 +0530

    initial commit

---
+++ src/main/java/tacos/Taco.java
@@ -0,0 +1,19 @@
+package tacos;
+
+import java.util.List;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class Taco {
+
+	@NotNull
+	@Size(min = 5, message = "Name must me at least 5 characters long")
+	private String name;
+	
+	@NotNull
+	@Size(min = 1, message = "You must choose at least 1 ingredient")
+	private List<Ingredient> ingredients;
+}
