d41dd2dbc9898581b2b4ed2195bfd0a8650ab787
Vishal Pawar <168279073+vishalvickypawar@users.noreply.github.com>
2025-08-13 21:46:15 +0530

    save taco order

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,7 +5,7 @@ create table if not exists Taco_Order(
 	delivery_City varchar(50) not null,
 	delivery_State varchar(2) not null,
 	delivery_Zip varchar(10) not null,
-	cc_number varchar(16) nor null,
+	cc_number varchar(16) not null,
 	cc_expiration varchar(5) not null,
 	cc_cvv varchar(3) not null,
 	placed_at timestamp not null
@@ -26,7 +26,7 @@ create table if not exists Ingredient_Ref(
 );
 
 create table if not exists Ingredient(
-	id varchar(4) not null,
+	id varchar(4) not null primary key,
 	name varchar(25) not null,
 	type varchar(10) not null
 );
