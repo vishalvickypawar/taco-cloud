01b0fac7af84ca7753eaca20421edc2d29a22bc7
Vishal Pawar <168279073+vishalvickypawar@users.noreply.github.com>
2025-08-15 21:42:40 +0530

    Spring JDBC

--- src/main/java/tacos/data/JdbcOrderRepository.java
+++ src/main/java/tacos/data/JdbcOrderRepository.java
@@ -12,6 +12,7 @@ import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import tacos.Ingredient;
 import tacos.Taco;
 import tacos.TacoOrder;
 
@@ -62,9 +63,29 @@ public class JdbcOrderRepository implements OrderRepository {
 		return order;
 	}
 
-	private void saveTaco(long orderId, int orderKey, Taco taco) {
+	private long saveTaco(long orderId, int orderKey, Taco taco) {
 		taco.setCreatedAt(new Date());
 		
+		PreparedStatementCreatorFactory pscf = new PreparedStatementCreatorFactory("insert into Taco (name, created_at, taco_order, taco_order_key) values (?,?,?,?)", Types.VARCHAR, Types.TIMESTAMP, Types.LONGVARCHAR, Types.LONGVARCHAR);
+		pscf.setReturnGeneratedKeys(true);
+		
+		PreparedStatementCreator psc = pscf.newPreparedStatementCreator(Arrays.asList(taco.getName(),taco.getCreatedAt(),orderId,orderKey));
+		
+		GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
+		jdbcOperations.update(psc, keyHolder);
+		long tacoId = keyHolder.getKey().longValue();
+		taco.setId(tacoId);
+		
+		saveIngredientRefs(tacoId,taco.getIngredients());
+		
+		return tacoId;
+	}
+
+	private void saveIngredientRefs(long tacoId, List<Ingredient> ingredients) {
+		int key=0;
+		for(Ingredient ingredient:ingredients) {
+			jdbcOperations.update("insert into Ingredient_Ref (ingredient, taco, taco_key) values (?,?,?)", ingredient.getId(), tacoId, key++);
+		}
 		
 	}
 
--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -11,12 +11,19 @@ import org.springframework.web.bind.support.SessionStatus;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import tacos.TacoOrder;
+import tacos.data.OrderRepository;
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
 public class OrderController {
+	
+	private OrderRepository orderRepository;
+
+	public OrderController(OrderRepository orderRepository) {
+		this.orderRepository = orderRepository;
+	}
 
 	@GetMapping("/current")
 	public String orderForm() {
@@ -28,6 +35,7 @@ public class OrderController {
 		if(errors.hasErrors()) {
 			return "orderForm";
 		}
+		orderRepository.save(order);
 		log.info("Order submitted: {}", order);
 		sessionStatus.setComplete();
 		return "redirect:/";
